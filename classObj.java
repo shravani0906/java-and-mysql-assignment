5abc3b8202c3a7b8820cc618d59a6eafc97292fe
shravani0906 <shravanisuryawanshi530@gmail.com>
2025-07-01 19:11:07 +0530

    Create classObj.java

---
+++ classObj.java
@@ -0,0 +1,17 @@
+
+public class ClassObj {
+
+    public void method1() {
+        System.out.println("Hello, this is a method in ClassObj.");
+    }
+    public void umesh() {
+        System.out.println("This is show method in ClassObj.");
+    }
+    public static void main(String[] args) {
+        //create an instance of ClassObj
+        ClassObj obj = new ClassObj();
+        obj.method1();
+        obj.umesh();
+
+    }
+}
