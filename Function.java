33e010c9d38e8fa637fc76cddd7e1f3fe0418e7a
shravani0906 <shravanisuryawanshi530@gmail.com>
2025-07-01 19:14:18 +0530

    Create Function.java

---
+++ Function.java
@@ -0,0 +1,13 @@
+
+
+public class Function {
+    public static int multiplication(int a, int b) {
+        return a * b;
+    }
+
+    public static void main(String[] args) {
+        int c;
+        c = multiplication(2, 3);
+        System.out.println(c); 
+    }
+}
