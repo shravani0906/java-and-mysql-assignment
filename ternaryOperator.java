242b047b7998daf2570d7dfcc3b3065a7c7c4fc8
shravani0906 <shravanisuryawanshi530@gmail.com>
2025-07-01 19:02:16 +0530

    Create ternaryOperator.java

---
+++ ternaryOperator.java
@@ -0,0 +1,11 @@
+
+public class ternaryOperator {
+    public static void main(String[] args) {
+        int number = 9;
+
+        String result = (number % 2 == 0) ? "Even" : "Odd";
+
+        System.out.println(number + " is " + result);
+    }
+    
+}
