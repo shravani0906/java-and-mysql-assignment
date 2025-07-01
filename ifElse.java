bc7f692b4773b7e377ea098623e4df3ce4a76407
shravani0906 <shravanisuryawanshi530@gmail.com>
2025-07-01 19:07:16 +0530

    Create ifElse.java

---
+++ ifElse.java
@@ -0,0 +1,16 @@
+
+
+public class ifElse {
+   public ifElse() {
+   }
+
+   public static void main(String[] var0) {
+      byte var1 = 8;
+      if (var1 % 2 == 0) {
+         System.out.println("" + var1 + " is Even");
+      } else {
+         System.out.println("" + var1 + " is Odd");
+      }
+
+   }
+}
