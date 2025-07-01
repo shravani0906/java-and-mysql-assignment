b6b05c07a1935cea41c79cfc35f31964d33ae13a
shravani0906 <shravanisuryawanshi530@gmail.com>
2025-07-01 19:04:16 +0530

    Create operator.java

---
+++ operator.java
@@ -0,0 +1,13 @@
+
+
+public class operator {
+    public static void main(String[] args){
+        int a = 2;
+        int b = 3;
+        
+        a+=b;
+        System.out.println(a);
+
+    }
+    
+}
